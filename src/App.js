e0ed932dcc1ad9d5261c1b580d439db2d6dc324b
tupy77 <tupy77@hotmail.com>
2023-07-30 22:58:40 +0200

    inst SEMANTIC UI REACT

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
 export default function App() {
   return (
     <div>
       <h1>Hola Musicfy</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   );
 }
