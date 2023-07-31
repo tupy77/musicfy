c05089efeb3ea57fb744eb94bfc49d428e9965ee
tupy77 <tupy77@hotmail.com>
2023-07-31 14:40:10 +0200

    LOGIN FORM, estilos

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,18 +1,47 @@
-import React from "react";
-import { Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Form, Icon } from "semantic-ui-react";
+import "./LoginForm.scss";
 
 export function LoginForm(props) {
-  const { openLoginForm, goBack } = props;
-  return (
-    <div style={{ backgroundColor: "#f0f" }}>
-      <h1>LoginForm</h1>
+  const { openRegisterForm, goBack } = props;
 
-      <Button primary onClick={openLoginForm}>
-        Log in
-      </Button>
-      <Button secondary onClick={goBack}>
-        Back
-      </Button>
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onShowHidePassword = () => setShowPassword(!showPassword);
+
+  return (
+    <div className="login-form">
+      <h1>Música para todos</h1>
+
+      <Form className="login-form__form">
+        <Form.Input
+          type="text"
+          placeholder="Correo electrónico"
+          icon={<Icon name="mail outline" />}
+        />
+        <Form.Input
+          type={showPassword ? "text" : "password"}
+          placeholder="Contraseña"
+          icon={
+            <Icon
+              name={showPassword ? "eye slash" : "eye"}
+              link
+              onClick={onShowHidePassword}
+            />
+          }
+        />
+
+        <Form.Button type="submit" primary fluid>
+          Iniciar sesión
+        </Form.Button>
+      </Form>
+
+      <div className="login-form__options">
+        <p onClick={goBack}>Volver</p>
+        <p>
+          ¿No tienes cuenta? <span onClick={openRegisterForm}>Regístrate</span>
+        </p>
+      </div>
     </div>
   );
 }
