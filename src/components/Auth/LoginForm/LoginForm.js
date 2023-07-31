95ecfbe7927d5330672d8563a6cf05e26dda7070
tupy77 <tupy77@hotmail.com>
2023-07-31 14:52:03 +0200

    LOGIN-control y validacion

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Form, Icon } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./LoginForm.scss";
 
 export function LoginForm(props) {
@@ -9,17 +11,31 @@ export function LoginForm(props) {
 
   const onShowHidePassword = () => setShowPassword(!showPassword);
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    // validateOnChange: false,
+    onSubmit: async (formData) => {
+      console.log("Formulario Enviado", formData);
+    },
+  });
+
   return (
     <div className="login-form">
       <h1>Música para todos</h1>
 
-      <Form className="login-form__form">
+      <Form className="login-form__form" onSubmit={formik.handleSubmit}>
         <Form.Input
+          name="email"
           type="text"
           placeholder="Correo electrónico"
           icon={<Icon name="mail outline" />}
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          error={formik.errors.email && true}
         />
         <Form.Input
+          name="password"
           type={showPassword ? "text" : "password"}
           placeholder="Contraseña"
           icon={
@@ -29,9 +45,12 @@ export function LoginForm(props) {
               onClick={onShowHidePassword}
             />
           }
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          error={formik.errors.password && true}
         />
 
-        <Form.Button type="submit" primary fluid>
+        <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
           Iniciar sesión
         </Form.Button>
       </Form>
@@ -44,4 +63,18 @@ export function LoginForm(props) {
       </div>
     </div>
   );
+}
+
+function initialValues() {
+  return {
+    email: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return Yup.object({
+    email: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  });
 }
