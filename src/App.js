c883bdaf309273b2c378332ff14a204663895a12
tupy77 <tupy77@hotmail.com>
2023-07-31 02:42:29 +0200

    Navegacion Usuario Logeado y Test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   const auth = getAuth();
 
   onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
+    setUser({ name: "Teste" });
   });
 
   if (user === undefined) return null;
--- src/routes/LoggedNavigation.js
+++ src/routes/LoggedNavigation.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Album, Albums, Artist, Artists, Home, Profile } from "../pages";
 
 export function LoggedNavigation() {
   return (
-    <div>
-      <h1>LoggedNavigation</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/artists/:id" element={<Artist />} />
+        <Route path="/albums" element={<Albums />} />
+        <Route path="/albums/:id" element={<Album />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
