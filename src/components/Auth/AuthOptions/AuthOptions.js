8328d53c6938523255691b7df8987ee8309c9546
tupy77 <100968748+tupy77@users.noreply.github.com>
2023-07-31 10:03:59 +0200

    opciones registro y login

--- src/components/Auth/AuthOptions/AuthOptions.js
+++ src/components/Auth/AuthOptions/AuthOptions.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
 
+import "./AuthOptions.scss";
+
 export function AuthOptions(props) {
   const { openLoginForm, openRegisterForm } = props;
 
   return (
-    <div>
-      <h1>AuthOptions</h1>
-      <Button primary onClick={openRegisterForm}>
-        Registro
+    <div className="auth-options">
+      <h1>Millones de canciones, gratis en Musicfy</h1>
+      <Button className="register" onClick={openRegisterForm}>
+        Registrate gratis
       </Button>
-      <Button primary onClick={openLoginForm}>
-        Login
+      <Button className="login" onClick={openLoginForm}>
+        Iniciar sesion
       </Button>
     </div>
   );
