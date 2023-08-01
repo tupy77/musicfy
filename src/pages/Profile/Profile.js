6337755b8e2a7e10d4ec79f4d3af649240b617d1
tupy77 <100968748+tupy77@users.noreply.github.com>
2023-08-01 12:18:05 +0200

    AJUSTES USUARIO Estructura Estilos

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
+import { User } from "../../api";
+import { defaultUser } from "../../assets";
+import "./Profile.scss";
+
+const user = new User();
 
 export function Profile() {
+  const userData = user.getMe();
+
+  const { displayName, email, photoURL } = userData;
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <h1>Configuracion</h1>
+
+      <div className="profile__block">
+        <div>
+          <p>{photoURL || defaultUser}</p>
+          <span>{displayName || "Sin nombre"}</span>
+        </div>
+        <Button primary onClick={() => console.log("Cambiar displayName")}>
+          Actualizar Nombre
+        </Button>
+      </div>
+
+      <div className="profile__block">
+        <div>
+          <span>Email: {email}</span>
+        </div>
+        <Button primary onClick={() => console.log("Cambiar Email")}>
+          Actualizar Email
+        </Button>
+      </div>
+
+      <div className="profile__block">
+        <div>
+          <span>Contraseña</span>
+        </div>
+        <Button primary onClick={() => console.log("Cambiar Email")}>
+          Actualizar Contraseña
+        </Button>
+      </div>
     </div>
   );
 }
