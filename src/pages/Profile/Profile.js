b624127de41fd578ec4b7f36b824e9d3c795f680
tupy77 <100968748+tupy77@users.noreply.github.com>
2023-08-02 10:51:44 +0200

    PROFILE MODAL Controlar contenido

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "semantic-ui-react";
 import { AvatarUpdate } from "../../components";
 import { BasicModal } from "../../components/Shared";
@@ -8,9 +8,40 @@ import "./Profile.scss";
 const user = new User();
 
 export function Profile() {
-  const userData = user.getMe();
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState("");
+  const [contentModal, setContentModal] = useState(null);
 
+  const userData = user.getMe();
   const { displayName, email } = userData;
+
+  const onCloseModal = () => {
+    setShowModal(false);
+    setContentModal(null);
+    setTitleModal("");
+  };
+
+  const openForm = (type) => {
+    switch (type) {
+      case "name":
+        setTitleModal("Actualizar Nombre");
+        setContentModal(<h2>Formulario para actualizar nombre</h2>);
+        setShowModal(true);
+        break;
+      case "email":
+        setTitleModal("Actualizar Email");
+        setContentModal(<h2>Formulario para actualizar email</h2>);
+        setShowModal(true);
+        break;
+      case "password":
+        setTitleModal("Actualizar Contraseña");
+        setContentModal(<h2>Formulario para actualizar contraseña</h2>);
+        setShowModal(true);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <>
@@ -22,7 +53,7 @@ export function Profile() {
             <AvatarUpdate />
             <span>{displayName || "Sin nombre"}</span>
           </div>
-          <Button primary onClick={() => console.log("Cambiar displayName")}>
+          <Button primary onClick={() => openForm("name")}>
             Actualizar Nombre
           </Button>
         </div>
@@ -31,7 +62,7 @@ export function Profile() {
           <div>
             <span>Email: {email}</span>
           </div>
-          <Button primary onClick={() => console.log("Cambiar Email")}>
+          <Button primary onClick={() => openForm("email")}>
             Actualizar Email
           </Button>
         </div>
@@ -40,17 +71,17 @@ export function Profile() {
           <div>
             <span>Contraseña</span>
           </div>
-          <Button primary onClick={() => console.log("Cambiar Email")}>
+          <Button primary onClick={() => openForm("password")}>
             Actualizar Contraseña
           </Button>
         </div>
       </div>
 
       <BasicModal
-        show={true}
-        onClose={() => console.log("Cerrar modal")}
-        title="Actualizar datos"
-        children={<h1>Contenido del modal</h1>}
+        show={showModal}
+        onClose={onCloseModal}
+        title={titleModal}
+        children={contentModal}
       />
     </>
   );
