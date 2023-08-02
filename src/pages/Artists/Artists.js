05bc87b8aa01b637afd7006311179c68c71ef7b9
tupy77 <tupy77@hotmail.com>
2023-08-02 20:20:34 +0200

    OBTENER TODOS LOS ARTISTAS

--- src/pages/Artists/Artists.js
+++ src/pages/Artists/Artists.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Artist } from "../../api";
+
+const artistController = new Artist();
 
 export function Artists() {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await artistController.obtainAll();
+        setArtists(response);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Artists</h1>
