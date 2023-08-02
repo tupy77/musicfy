57d78064671d8c281388d76ee83f5ba15a949a93
tupy77 <tupy77@hotmail.com>
2023-08-02 23:35:18 +0200

    FORM ADD ALBUM Validaciones

--- src/components/Albums/AddAlbumForm/AddAlbumForm.js
+++ src/components/Albums/AddAlbumForm/AddAlbumForm.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Form, Image } from "semantic-ui-react";
 import classNames from "classnames";
-import { useFormik } from "formik";
 import * as Yup from "yup";
+import { useFormik } from "formik";
 import "./AddAlbumForm.scss";
 
 export function AddAlbumForm(props) {
   const { onClose } = props;
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
-    <Form className="add-album-form">
+    <Form className="add-album-form" onSubmit={formik.handleSubmit}>
       <div className="add-album-form__content">
         <div
           className={classNames("add-album-form__content-image", {
@@ -20,7 +28,13 @@ export function AddAlbumForm(props) {
         </div>
 
         <div className="add-album-form__content-inputs">
-          <Form.Input name="name" placeholder="nombre del album" />
+          <Form.Input
+            name="name"
+            placeholder="nombre del album"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.errors.name}
+          />
 
           <Form.Dropdown
             placeholder="Elige un artista"
@@ -32,9 +46,25 @@ export function AddAlbumForm(props) {
         </div>
       </div>
 
-      <Form.Button type="submit" fluid primary>
+      <Form.Button type="submit" fluid primary loading={formik.isSubmitting}>
         Crear album
       </Form.Button>
     </Form>
   );
+}
+
+function initialValues() {
+  return {
+    image: null,
+    name: "",
+    artist: "",
+  };
+}
+
+function validationSchema() {
+  return Yup.object({
+    image: Yup.string().required(true),
+    name: Yup.string().required(true),
+    artist: Yup.string().required(true),
+  });
 }
