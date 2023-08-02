2254091eb65697a30505ca4830eb63edb5b3343b
tupy77 <100968748+tupy77@users.noreply.github.com>
2023-08-02 11:11:05 +0200

    FORM DISPLAYNAME Controlar y validar datos

--- src/components/Profile/DisplayNameUpdateForm/DisplayNameUpdateForm.js
+++ src/components/Profile/DisplayNameUpdateForm/DisplayNameUpdateForm.js
@@ -1,15 +1,47 @@
 import React from "react";
 import { Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import { User } from "../../../api";
+import * as Yup from "yup";
+
+const userController = new User();
 
 export function DisplayNameUpdateForm(props) {
   const { onClose } = props;
+  const { displayName } = userController.getMe();
+
+  const formik = useFormik({
+    initialValues: initialValues(displayName),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
 
   return (
-    <Form>
-      <Form.Input name="displayName" placeholder="Nombre y Apellidos" />
-      <Form.Button type="submit" primary fluid>
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="displayName"
+        placeholder="Nombre y Apellidos"
+        value={formik.values.displayName}
+        onChange={formik.handleChange}
+        error={formik.errors.displayName}
+      />
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         Actualizar
       </Form.Button>
     </Form>
   );
+}
+
+function initialValues(name) {
+  return {
+    displayName: name || "",
+  };
+}
+
+function validationSchema() {
+  return {
+    displayName: Yup.string().required(true),
+  };
 }
