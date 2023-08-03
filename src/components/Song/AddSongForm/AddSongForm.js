f5857d74bc5bb2252f56ad09c8a16e0784b4914a
tupy77 <tupy77@hotmail.com>
2023-08-03 18:55:31 +0200

    UPLOADSONGFORM Control y validacion

--- src/components/Song/AddSongForm/AddSongForm.js
+++ src/components/Song/AddSongForm/AddSongForm.js
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
 import { Form, Icon } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./AddSongForm.scss";
 import classNames from "classnames";
 
 export function AddSongForm() {
   const [songName, setSongName] = useState("");
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
-    <Form className="add-song-form">
-      <Form.Input name="name" placeholder="Nombre de la cación" />
+    <Form className="add-song-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="name"
+        placeholder="Nombre de la canción"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+        error={formik.errors.name}
+      />
       <Form.Dropdown
         placeholder="Asigna la canción a un álbum"
         fluid
@@ -31,9 +47,25 @@ export function AddSongForm() {
         </div>
       </div>
 
-      <Form.Button type="submit" primary fluid>
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         Subir canción
       </Form.Button>
     </Form>
   );
+}
+
+function initialValues() {
+  return {
+    name: "",
+    album: "",
+    file: null,
+  };
+}
+
+function validationSchema() {
+  return {
+    name: Yup.string().required(true),
+    album: Yup.string().required(true),
+    file: Yup.string().required(true),
+  };
 }
