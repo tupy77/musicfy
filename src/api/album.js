3116f4c8a4bd30f75d9c27075835ac5c8082ee02
tupy77 <100968748+tupy77@users.noreply.github.com>
2023-08-03 10:53:14 +0200

    OBTENER TODOS LOS ALBUMS

--- src/api/album.js
+++ src/api/album.js
@@ -1,5 +1,6 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDocs, collection } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
+import { map } from "lodash";
 import { db } from "../utils";
 
 export class Album {
@@ -13,6 +14,22 @@ export class Album {
 
       const docRef = doc(db, this.collectionName, id);
       await setDoc(docRef, data);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async obtainAll() {
+    try {
+      const collectionRef = collection(db, this.collectionName);
+      const snapshot = await getDocs(collectionRef);
+      const data = [];
+      // snapshot.forEach((doc) => {
+      //   data.push(doc.data());
+      // });
+      // return data;
+
+      return map(snapshot.docs, (doc) => doc.data());
     } catch (error) {
       throw error;
     }
--- src/pages/Albums/Albums.js
+++ src/pages/Albums/Albums.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Album } from "../../api";
+
+const albumController = new Album();
 
 export function Albums() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await albumController.obtainAll();
+        console.log(response);
+        setAlbums(response);
+      } catch (error) {
+        throw error;
+      }
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Albums</h1>
