a61fb81039991cebd0246971c93bc177888e794c
tupy77 <100968748+tupy77@users.noreply.github.com>
2023-08-04 12:23:19 +0200

    *OBTENER CANCIONES DE UN ARTISTA

--- src/pages/Artist/Artist.js
+++ src/pages/Artist/Artist.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; //PARA OBTENER LOS PARAMETROS DE LA URL
-import { Artist as ArtistController, Album } from "../../api";
+import { Artist as ArtistController, Album, Song } from "../../api";
+import { map } from "lodash";
 import { ArtistBanner } from "../../components/Artist";
-import { Slider } from "../../components/Shared";
+import { Slider } from "../../components";
 
 import "./Artist.scss";
 
 const artistController = new ArtistController();
 const albumController = new Album();
+const songController = new Song();
 
 export function Artist(props) {
   // const params = useParams();
@@ -15,6 +17,7 @@ export function Artist(props) {
   const { id } = useParams();
   const [artists, setArtists] = useState(null);
   const [albums, setAlbums] = useState(null);
+  const [songs, setSongs] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -38,6 +41,34 @@ export function Artist(props) {
       }
     })();
   }, [id]);
+
+  useEffect(() => {
+    if (albums) {
+      (async () => {
+        try {
+          const arrayAlbums = [];
+          for await (const album of albums) {
+            const response = await songController.obtainAllByAlbum(album.id);
+            const dataTemp = map(response, (song) => {
+              return {
+                ...song,
+                album: album.name,
+                albumImage: album.image,
+              };
+            });
+
+            arrayAlbums.push(...dataTemp); //Los tres puntos son para tener un array de objetos, no un array de arrays
+            // arrayAlbums.push(response);
+          }
+
+          console.log(arrayAlbums);
+          setSongs(arrayAlbums);
+        } catch (error) {
+          console.error(error);
+        }
+      })();
+    }
+  }, [albums]);
 
   if (!artists) return null;
 
