62fe0228b4a308419f875e84a5b639d69b908d1c
tupy77 <tupy77@hotmail.com>
2023-08-05 07:43:40 +0200

    IMPLEMENTAR REPRODUCTOR

--- src/components/Shared/Player/Player.js
+++ src/components/Shared/Player/Player.js
@@ -1,14 +1,24 @@
 import React from "react";
+import ReactPlayer from "react-player";
+import { usePlayer } from "../../../hooks";
 import { Progress, Icon } from "semantic-ui-react";
 import "./Player.scss";
 
 export function Player() {
-  const playing = true;
+  const { song, playing, pause, volume, resume } = usePlayer();
 
   return (
     <div className="player">
       <Icon name={playing ? "pause circle outline" : "play circle outline"} />
       <Progress progress="value" value={30} total={100} size="tiny" />
+
+      <ReactPlayer
+        url={song?.file}
+        playing={playing}
+        volume={volume}
+        height={0}
+        width={0}
+      />
     </div>
   );
 }
